ff6e8ef6d9b51105154364d22597c7b52cc5af98
beau.furrow <beau.furrow@bluemedora.com>
2016-11-05 15:56:45 -0400

    Uncommented sinatra/schedulere code.

--- petfinder.rb
+++ petfinder.rb
@@ -4,7 +4,7 @@ Dotenv.load
 require 'json'
 require 'petfinder'
 require 'rufus-scheduler'
-# require 'sinatra'
+require 'sinatra'
 require 'sequel'
 require 'pg'
 
@@ -15,18 +15,18 @@ petfinder = Petfinder::Client.new(api_key, api_secret)
 
 
 
-# get '/pets' do
-#   pets = petfinder.find_pets('cat', '49505')
-#   names = pets.map { |pet| pet.name }
-#   {:names => names}.to_json
-# end
+get '/pets' do
+  pets = petfinder.find_pets('cat', '49505')
+  names = pets.map { |pet| pet.name }
+  {:names => names}.to_json
+end
 
-# get '/pets/:shelter_id' do
-#   options = {count:1000}
-#   pets = petfinder.shelter_pets(params['shelter_id'], options)
-#   names = pets.map { |pet| pet.names}
-#   {:name => names}.to_json
-# end
+get '/pets/:shelter_id' do
+  options = {count:1000}
+  pets = petfinder.shelter_pets(params['shelter_id'], options)
+  names = pets.map { |pet| pet.names}
+  {:name => names}.to_json
+end
 
 def init_shelter_ids
   ENV['PETFINDER_SHELTER_IDS'].split(',')
@@ -108,15 +108,15 @@ shelter_ids = init_shelter_ids()
 
 scheduler = Rufus::Scheduler.new
 
-# scheduler.every '1h' do
+scheduler.every '1h' do
   shelter_ids.each do |id|
     add_shelter(petfinder.shelter(id))
 
-    pets = petfinder.shelter_pets(id, {count: 5})
+    pets = petfinder.shelter_pets(id, {count: 1000})
     add_pet(pets.first)
-    # pets.each do |pet|
-    #   add_pet(pet)
-    # end
-    # store pets
+    pets.each do |pet|
+      add_pet(pet)
+    end
+    store pets
   end
-# end
+end

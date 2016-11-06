43c4139dc64e9981d7790eb8a707f2984e96337d
Ross-Hunter <ross-hunter@ross-hunter.com>
2016-11-05 21:27:28 -0400

    Use bootstrap nom package

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-import './bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <App />,
